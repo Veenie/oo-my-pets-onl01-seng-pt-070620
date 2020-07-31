c93e62ca87e64de390bba6bd0da0a0b497db1d78
Raveen K Sharma <raveen08@gmail.com>
2020-07-31 18:37:07 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,19 @@
 class Owner
+  attr_accessor :name
+  
+  @@pets
+  
+  def initialize (name)
+    @name = name
+  end
+  
+  def buy_cat
+  end
+  
+  def buy_dog
+  end
+  
+  
+  
   # code goes here
 end
